bfafabcdf4f1f70e87521a7b9ab02915a6472de8
Elzida <152496247+Elzida@users.noreply.github.com>
2023-12-13 23:48:44 +0600

    Initial commit

---
+++ src/FizzBuzz.java
@@ -0,0 +1,10 @@
+public class FizzBuzz {
+    public static void main(String[] args) {
+        int[] numbers = new int[5];
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = i + 1;
+            System.out.println(numbers[i]);
+        }
+
+    }
+}
