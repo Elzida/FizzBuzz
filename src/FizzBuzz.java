29c605223f6d4e27acca85a965b069431f757033
Elzida <152496247+Elzida@users.noreply.github.com>
2023-12-15 20:14:21 +0600

    Update FizzBuzz.java

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,10 +1,20 @@
 public class FizzBuzz {
     public static void main(String[] args) {
-        int[] numbers = new int[5];
+        int [] numbers = new int[20];
         for (int i = 0; i < numbers.length; i++) {
             numbers[i] = i + 1;
+            if(numbers[i] % 5==0 && numbers[i] % 3==0) {
+                numbers[i]= 8888;
+            }
+            else if(numbers[i] % 3==0) {
+
+                numbers[i]= 1055;}
+            else if(numbers[i] % 5==0) {
+                numbers[i]= 1033;
+            }
             System.out.println(numbers[i]);
         }
+
 
     }
 }
